9dc3d11ba8a37e9f33cf5a03b1164b09bff3cc69
Regis Bruggheman <regis@joufdesign.com>
2014-10-11 09:39:00 +0200

    bump

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140111110444) do
+ActiveRecord::Schema.define(:version => 20141011073460) do
+
   create_table "assets", :force => true do |t|
     t.integer  "site_id"
     t.string   "content_type"
@@ -43,7 +44,6 @@ ActiveRecord::Schema.define(:version => 20140111110444) do
   create_table "contents", :force => true do |t|
     t.integer  "site_id"
     t.integer  "section_id"
-    t.integer  "account_id"
     t.string   "type"
     t.string   "title"
     t.string   "slug"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(:version => 20140111110444) do
   add_index "contents", ["slug"], :name => "index_contents_on_slug"
 
   create_table "document_assignments", :force => true do |t|
-    t.integer  "position",                      :default => 1, :null => false
+    t.integer  "position",                      :default => 1
     t.integer  "document_id",                                  :null => false
     t.integer  "attachable_id",                                :null => false
     t.string   "attachable_type", :limit => 40,                :null => false
@@ -132,7 +132,7 @@ ActiveRecord::Schema.define(:version => 20140111110444) do
   end
 
   create_table "image_assignments", :force => true do |t|
-    t.integer  "position",                      :default => 1, :null => false
+    t.integer  "position",                      :default => 1
     t.integer  "image_id",                                     :null => false
     t.integer  "attachable_id",                                :null => false
     t.string   "attachable_type", :limit => 40,                :null => false
@@ -174,6 +174,7 @@ ActiveRecord::Schema.define(:version => 20140111110444) do
     t.integer  "image_width"
     t.integer  "image_height"
     t.string   "image_uid"
+    t.string   "video_url"
   end
 
   create_table "inquiries", :force => true do |t|
